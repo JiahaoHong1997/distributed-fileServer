de29c1a1c7fde33e49f3678d3f05d61ff6c29b2f
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-06-06 19:08:40 +0800

    upload handler

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ import (
 )
 
 func main() {
+
+	// 静态资源处理
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("./static"))))
+
 	http.HandleFunc("/file/upload", handler.UploadHandler)
+
+	// 监听端口
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Fail to start server, err:%s\n", err.Error())
