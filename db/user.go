6017b9c569e554c4035bba2abd4664296d7fbd3b
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-10-13 14:20:57 +0800

    实现用户信息查询接口

--- db/user.go
+++ db/user.go
@@ -65,3 +65,31 @@ func UpdateToken(userName string, token string) bool {
 	}
 	return true
 }
+
+type User struct {
+	Username     string
+	Email        string
+	Phone        string
+	SignupAt     string
+	LastActiveAt string
+	Status       int
+}
+
+// GetUserInfo:用户信息查询
+func GetUserInfo(userName string) (User, error) {
+	user := User{}
+
+	stmt, err := mydb.DBConn().Prepare("select user_name,signup_at from tbl_user where user_name=? limit 1")
+	if err != nil {
+		fmt.Println(err.Error())
+		return user, err
+	}
+	defer stmt.Close()
+
+	// 执行查询操作
+	err = stmt.QueryRow(userName).Scan(&user.Username, &user.SignupAt)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
--- handler/user.go
+++ handler/user.go
@@ -15,6 +15,8 @@ const (
 
 // SignUpHandler:处理用户注册请求
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+
+	// 1.判断如果是http Get请求，直接返回注册页面内容
 	if r.Method == http.MethodGet {
 		data, err := ioutil.ReadFile("./static/view/signup.html")
 		if err != nil {
@@ -29,12 +31,16 @@ func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	userName := r.Form.Get("username")
 	passwd := r.Form.Get("password")
 
+	// 2.校验参数的有效性
 	if len(userName) < 3 || len(passwd) < 5 {
 		w.Write([]byte("Invilid parameter"))
 		return
 	}
 
+	// 3.加密用户名密码
 	enc_passwd := util.Sha1([]byte(passwd + pwd_salt))
+
+	// 4.存入数据库表tbl_user并返回结果
 	suc := dblayer.UserSignUp(userName, enc_passwd)
 	if suc {
 		w.Write([]byte("SUCCESS"))
@@ -60,7 +66,6 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	userName := r.Form.Get("username")
 	passWord := r.Form.Get("password")
-
 	encPasswd := util.Sha1([]byte(passWord + pwd_salt))
 
 	// 1.校验用户名及密码
@@ -72,14 +77,15 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 2.生成访问凭证（token）
 	token := GenToken(userName)
+
+	// 3.存储token到tbl_user_token表
 	upRes := dblayer.UpdateToken(userName, token)
 	if !upRes {
 		w.Write([]byte("Failed!"))
 		return
 	}
 
-	// 3.登录成功后重定向到首页
-	//w.Write([]byte("http://" + r.Host + "/static/view/home.html"))
+	// 4.登录成功后重定向到首页
 	resp := util.RespMsg{
 		Code: 0,
 		Msg:  "OK",
@@ -97,9 +103,55 @@ func SignInHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// UserInfoHandler:查新用户信息接口
+func UserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	// 1.解析请求参数
+	r.ParseForm()
+	userName := r.Form.Get("username")
+	token := r.Form.Get("token")
+
+	//2.验证token是否有效
+	isValidToken := IsTokenValid(token)
+	if !isValidToken {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
+	// 3.查询用户信息
+	user, err := dblayer.GetUserInfo(userName)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	// 4.组装并且响应用户数据
+	resp := util.RespMsg{
+		Code: 0,
+		Msg:  "OK",
+		Data: user,
+	}
+	w.Write(resp.JSONBytes())
+}
+
 func GenToken(userName string) string {
 	// 40位字符md5(userName+timestamp+token_salt)+timestamp[:8]
 	ts := fmt.Sprintf("%x", time.Now().Unix())
 	tokenPrefix := util.MD5([]byte(userName + ts + "_tokensalt"))
 	return tokenPrefix + ts[:8]
+}
+
+// IsTokenValid：token是否有效
+func IsTokenValid(token string) bool {
+	if len(token) != 40 {
+		return false
+	}
+	// 判断token的时效性，是否过期
+	// example，假设token的有效期为1天
+	//n := len(token)
+	//tokenTS := token[n-8:]
+	//if util.Hex2Dec(tokenTS) < time.Now().Unix()-86400 {
+	//	return false
+	//}
+	// TODO:从数据库表tbl-user_token查询对应的username的token信息
+	// TODO:对比两个token是否一致
+	return true
 }
