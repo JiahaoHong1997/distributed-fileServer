32cd9d1b0696700592f9097cba9f7378fdc97f6b
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-11-19 22:00:21 +0800

    upload_handler

--- handler/mpupload.go
+++ handler/mpupload.go
@@ -3,21 +3,26 @@ package handler
 import (
 	"distributed-fileServer/util"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"math"
 	"net/http"
+	"os"
+	"path"
 	"strconv"
+	"strings"
 	"time"
 
 	rPool "distributed-fileServer/cache/redis"
+	dblayer "distributed-fileServer/db"
 )
 
 // MutiPartUploadInfo:初始化信息
 type MutiPartUploadInfo struct {
-	FileHash	string
-	FileSize	int
-	UploadID	string
-	ChunkSize	int
-	ChunkCount	int
+	FileHash   string
+	FileSize   int
+	UploadID   string
+	ChunkSize  int
+	ChunkCount int
 }
 
 // 初始化分块上传
@@ -38,18 +43,101 @@ func InitialMultipartUploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 3.生成分块上传的初始化信息
 	upInfo := MutiPartUploadInfo{
-		FileHash: fileHash,
-		FileSize: fileSize,
-		UploadID: userName + fmt.Sprintf("%x", time.Now().UnixNano()),
-		ChunkSize: 5*1024*1024,	// 5MB
-		ChunkCount: int(math.Ceil(float64(fileSize)/(5*1024*1024))),
+		FileHash:   fileHash,
+		FileSize:   fileSize,
+		UploadID:   userName + fmt.Sprintf("%x", time.Now().UnixNano()),
+		ChunkSize:  5 * 1024 * 1024, // 5MB
+		ChunkCount: int(math.Ceil(float64(fileSize) / (5 * 1024 * 1024))),
 	}
 
 	// 4.将初始化信息写入Redis缓存
-	rConn.Do("HSET", "MP_"+upInfo.UploadID,"chunkcount",upInfo.ChunkCount)
-	rConn.Do("HSET", "MP_"+upInfo.UploadID,"filehash",upInfo.FileHash)
-	rConn.Do("HSET", "MP_"+upInfo.UploadID,"filesize",upInfo.FileSize)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "chunkcount", upInfo.ChunkCount)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filehash", upInfo.FileHash)
+	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filesize", upInfo.FileSize)
 
 	// 5.将响应初始化数据返回到客户端
-	w.Write(util.NewRespMsg(0,"OK",upInfo).JSONBytes())
+	w.Write(util.NewRespMsg(0, "OK", upInfo).JSONBytes())
+}
+
+// UploadPartHandler: 上传文件分块
+func UploadPartHandler(w http.ResponseWriter, r *http.Request) {
+	// 1.解析用户请求参数
+	r.ParseForm()
+	//userName := r.Form.Get("username")
+	uploadID := r.Form.Get("uploadid")
+	chunkIndex := r.Form.Get("index")
+
+	// 2.获得Redis连接池的连接
+	rConn := rPool.RedisPool().Get()
+	defer rConn.Close()
+
+	// 3.获得文件句柄，用于存储分块内容
+	fpath := "/data/" + uploadID + "/" + chunkIndex
+	os.MkdirAll(path.Dir(fpath),0744)
+	fd, err := os.Create(fpath)
+	if err != nil {
+		w.Write(util.NewRespMsg(-1, "Upload part failed", nil).JSONBytes())
+		return
+	}
+	defer fd.Close()
+
+	buf := make([]byte, 1024*1024) // 1MB
+	for {
+		n, err := r.Body.Read(buf)
+		fd.Write(buf[:n])
+		if err != nil {
+			break
+		}
+	}
+
+	// 4.更新Redis缓存状态
+	rConn.Do("HSET", "MP_"+uploadID, "chkidx_"+chunkIndex, 1)
+
+	// 5.返回处理结果到客户端
+	w.Write(util.NewRespMsg(0, "OK", nil).JSONBytes())
+}
+
+// CompleteUploadHandler: 通知上传合并
+func CompleteUploadHandler(w http.ResponseWriter, r *http.Request) {
+	// 1.解析请求参数
+	r.ParseForm()
+	uploadID := r.Form.Get("uploadid")
+	userName := r.Form.Get("username")
+	fileHash := r.Form.Get("filehash")
+	filseSize := r.Form.Get("filesize")
+	fileName := r.Form.Get("filename")
+	// 2.获得redis连接池的一个连接
+	rConn := rPool.RedisPool().Get()
+	defer rConn.Close()
+
+	// 3.通过uploadid查询redis并判断是否所有的分块完成上传
+	data, err := redis.Values(rConn.Do("HGETALL", "MP_"+uploadID))
+	if err != nil {
+		w.Write(util.NewRespMsg(-1, "complete upload failed", nil).JSONBytes())
+		return
+	}
+	totalCount := 0
+	chunkCount := 0
+	for i := 0; i < len(data); i += 2 {
+		k := string(data[i].([]byte))
+		v := string(data[i+1].([]byte))
+		if k == "chunkcount" {
+			totalCount, _ = strconv.Atoi(v)
+		} else if strings.HasPrefix(k, "chkidx_") && v == "1" {
+			chunkCount++
+		}
+	}
+	if totalCount != chunkCount {
+		w.Write(util.NewRespMsg(-2, "invalid request", nil).JSONBytes())
+		return
+	}
+	// 4.TODO：合并分块
+
+	// 5.更新唯一文件表积用户文件表
+	fsize, _ := strconv.Atoi(filseSize)
+	dblayer.OnFileUploadFinished(fileHash, fileName, int64(fsize), "")
+	dblayer.OnUserFileUploadFinished(userName, fileHash, fileName, int64(fsize))
+
+	// 6.响应处理结果
+	w.Write(util.NewRespMsg(0, "OK", nil).JSONBytes())
 }
--- main.go
+++ main.go
@@ -13,17 +13,23 @@ func main() {
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/file/upload", handler.UploadHandler)         // 处理上传文件
-	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)  // 上传完成
-	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)      // 获取上传文件元信息
-	http.HandleFunc("/file/query", handler.FileQueryHandler)       // 批量获取上传文件的元信息
-	http.HandleFunc("/file/download", handler.DownloadHandler)     // 文件下载
-	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler) // 更新文件元信息(重命名)
-	http.HandleFunc("/file/delete", handler.FileDeleteHandler)     // 删除文件以及文件元信息
-	http.HandleFunc("/file/fastupload",handler.HTTPInterceptor(handler.TryFastUploadHandler))  // 尝试秒传的接口
+	http.HandleFunc("/file/upload", handler.UploadHandler)                                     // 处理上传文件
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)                              // 上传完成
+	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)                                  // 获取上传文件元信息
+	http.HandleFunc("/file/query", handler.FileQueryHandler)                                   // 批量获取上传文件的元信息
+	http.HandleFunc("/file/download", handler.DownloadHandler)                                 // 文件下载
+	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)                             // 更新文件元信息(重命名)
+	http.HandleFunc("/file/delete", handler.FileDeleteHandler)                                 // 删除文件以及文件元信息
+	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler)) // 尝试秒传的接口
 
-	http.HandleFunc("/user/signup", handler.SignUpHandler) // 用户注册
-	http.HandleFunc("/user/signin", handler.SignInHandler) // 用户登录
+	// 分块上传接口
+	http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler)) // 初始化分块上传
+	http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))           // 上传文件分块
+	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))    // 通知完成上传接口
+
+	// 用户相关接口
+	http.HandleFunc("/user/signup", handler.SignUpHandler)                          // 用户注册
+	http.HandleFunc("/user/signin", handler.SignInHandler)                          // 用户登录
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler)) // 用户信息查询
 
 	// 监听端口
