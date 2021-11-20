c4d3c5be994f2955041df5d212c2cf4bc538f7b2
JiahaoHong1997 <hongjiahao1997@gmail.com>
2021-11-20 18:35:18 +0800

    complete_MP

--- handler/mpupload.go
+++ handler/mpupload.go
@@ -16,13 +16,37 @@ import (
 	dblayer "distributed-fileServer/db"
 )
 
+const (
+	// ChunkDir：上传的分块所在目录
+	ChunkDir = "/data/chunks"
+	// MergeDir：合并后的文件所在目录
+	MergeDir = "/data/merge"
+	// ChunkKeyPrefix：分块信息对应的Redis键前缀
+	ChunkKeyPrefix = "MP_"
+	// HashUpIDKeyPrefix：文件hash映射uploadid随营的redis键前缀
+	HashUpIDKeyPrefix = "HASH_UPID_"
+)
+
+func init() {
+	if err := os.MkdirAll(ChunkDir,0744);err != nil {
+		fmt.Println("无法指定目录用于存储分块文件"+ChunkDir)
+		os.Exit(1)
+	}
+
+	if err := os.MkdirAll(MergeDir,0744);err != nil {
+		fmt.Println("无法指定目录用于存储合并后文件"+ChunkDir)
+		os.Exit(1)
+	}
+}
+
 // MutiPartUploadInfo:初始化信息
-type MutiPartUploadInfo struct {
-	FileHash   string
-	FileSize   int
-	UploadID   string
-	ChunkSize  int
-	ChunkCount int
+type MultiPartUploadInfo struct {
+	FileHash    string
+	FileSize    int
+	UploadID    string
+	ChunkSize   int
+	ChunkCount  int
+	ChunkExists []int // 已经上传完成的分块索引列表
 }
 
 // 初始化分块上传
@@ -41,21 +65,59 @@ func InitialMultipartUploadHandler(w http.ResponseWriter, r *http.Request) {
 	rConn := rPool.RedisPool().Get()
 	defer rConn.Close()
 
-	// 3.生成分块上传的初始化信息
-	upInfo := MutiPartUploadInfo{
+	// 3.通过文件hash判断是否断点续传，并获取uploadid
+	uploadID := ""
+	keyExists,_ := redis.Bool(rConn.Do("EXISTS", HashUpIDKeyPrefix+fileHash))
+	if keyExists {
+		uploadID, err = redis.String(rConn.Do("GET", HashUpIDKeyPrefix+fileHash))
+		if err != nil {
+			w.Write(util.NewRespMsg(-1,"Upload part failed",err.Error()).JSONBytes())
+			return
+		}
+	}
+
+	// 4.1 首次上传则新建uploaID
+	// 4.2 断点续传则根据uploadIB获取已经上传的文件分块列表
+	chunksExist := []int{}
+	if uploadID == "" {
+		uploadID = userName + fmt.Sprintf("%x", time.Now().UnixNano())
+	} else {
+		chunks,err := redis.Values(rConn.Do("HGETALL", ChunkKeyPrefix+uploadID)) // 读出已经上传的分块列表
+		if err != nil {
+			w.Write(util.NewRespMsg(-2, "Upload part failed", err.Error()).JSONBytes())
+			return
+		}
+		for i:=0; i<len(chunks); i+=2 {
+			k := string(chunks[i].([]byte))
+			v := string(chunks[i+1].([]byte))
+			if strings.HasPrefix(k, "chkidx_") && v == "1" {
+				chunkIdx,_ := strconv.Atoi(k[7:len(k)])
+				chunksExist = append(chunksExist,chunkIdx)
+			}
+		}
+	}
+
+	// 5.生成分块上传的初始化信息
+	upInfo := MultiPartUploadInfo{
 		FileHash:   fileHash,
 		FileSize:   fileSize,
 		UploadID:   userName + fmt.Sprintf("%x", time.Now().UnixNano()),
 		ChunkSize:  5 * 1024 * 1024, // 5MB
 		ChunkCount: int(math.Ceil(float64(fileSize) / (5 * 1024 * 1024))),
+		ChunkExists: chunksExist,
 	}
 
-	// 4.将初始化信息写入Redis缓存
-	rConn.Do("HSET", "MP_"+upInfo.UploadID, "chunkcount", upInfo.ChunkCount)
-	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filehash", upInfo.FileHash)
-	rConn.Do("HSET", "MP_"+upInfo.UploadID, "filesize", upInfo.FileSize)
+	// 6.首次上传的话将初始化信息写入Redis缓存
+	if len(upInfo.ChunkExists) <= 0 {
+		hkey := "MP_"+upInfo.UploadID
+		rConn.Do("HSET", hkey, "chunkcount", upInfo.ChunkCount)
+		rConn.Do("HSET", hkey, "filehash", upInfo.FileHash)
+		rConn.Do("HSET", hkey, "filesize", upInfo.FileSize)
+		rConn.Do("EXPIRE", hkey, 43200)
+		rConn.Do("SET", HashUpIDKeyPrefix+fileHash, upInfo.UploadID, "EX", 43200)
+	}
 
-	// 5.将响应初始化数据返回到客户端
+	// 7.将响应初始化数据返回到客户端
 	w.Write(util.NewRespMsg(0, "OK", upInfo).JSONBytes())
 }
 
@@ -73,7 +135,7 @@ func UploadPartHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 3.获得文件句柄，用于存储分块内容
 	fpath := "/data/" + uploadID + "/" + chunkIndex
-	os.MkdirAll(path.Dir(fpath),0744)
+	os.MkdirAll(path.Dir(fpath), 0744)
 	fd, err := os.Create(fpath)
 	if err != nil {
 		w.Write(util.NewRespMsg(-1, "Upload part failed", nil).JSONBytes())
@@ -106,6 +168,7 @@ func CompleteUploadHandler(w http.ResponseWriter, r *http.Request) {
 	fileHash := r.Form.Get("filehash")
 	filseSize := r.Form.Get("filesize")
 	fileName := r.Form.Get("filename")
+
 	// 2.获得redis连接池的一个连接
 	rConn := rPool.RedisPool().Get()
 	defer rConn.Close()
@@ -132,6 +195,10 @@ func CompleteUploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// 4.TODO：合并分块
+	if mergeSuc := util.MergeChuncksByShell(ChunkDir+uploadID, MergeDir+fileHash, fileHash); !mergeSuc {
+		w.Write(util.NewRespMsg(-3, "Complete upload failed", nil).JSONBytes())
+		return
+	}
 
 	// 5.更新唯一文件表积用户文件表
 	fsize, _ := strconv.Atoi(filseSize)
@@ -140,4 +207,38 @@ func CompleteUploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 6.响应处理结果
 	w.Write(util.NewRespMsg(0, "OK", nil).JSONBytes())
+}
+
+// CancelUploadHandler：文件取消上传接口
+func CancelUploadHandler(w http.ResponseWriter, r *http.Request) {
+	// 1.解析用户请求参数
+	r.ParseForm()
+	fileHash := r.Form.Get("filehash")
+
+	// 2.获得redis的一个连接
+	rConn := rPool.RedisPool().Get()
+	defer rConn.Close()
+
+	// 3.检查uploadid是否存在，如果存在则删除
+	uploadID,err := redis.String(rConn.Do("GET", HashUpIDKeyPrefix+fileHash))
+	if err != nil || uploadID == "" {
+		w.Write(util.NewRespMsg(-1,"Cancel upload part failed",nil).JSONBytes())
+		return
+	}
+
+	_,delHashErr := rConn.Do("DEL", HashUpIDKeyPrefix+fileHash)
+	_,delUploadInfoErr := rConn.Do("DEL", ChunkKeyPrefix+uploadID)
+	if delHashErr != nil || delUploadInfoErr != nil {
+		w.Write(util.NewRespMsg(-2,"Cancel upload part failed",nil).JSONBytes())
+		return
+	}
+
+	// 4.删除已经上传的分块文件
+	delChkRes := util.RemovePathByShell(ChunkDir+uploadID)
+	if !delChkRes {
+		fmt.Printf("Failed to delete chunks as upload canceled, uploadID:%s\n",uploadID)
+	}
+
+	// 5.响应客户端
+	w.Write(util.NewRespMsg(0,"OK",nil).JSONBytes())
 }
--- main.go
+++ main.go
@@ -13,19 +13,23 @@ func main() {
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/file/upload", handler.UploadHandler)                                     // 处理上传文件
-	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)                              // 上传完成
-	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)                                  // 获取上传文件元信息
-	http.HandleFunc("/file/query", handler.FileQueryHandler)                                   // 批量获取上传文件的元信息
-	http.HandleFunc("/file/download", handler.DownloadHandler)                                 // 文件下载
-	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)                             // 更新文件元信息(重命名)
-	http.HandleFunc("/file/delete", handler.FileDeleteHandler)                                 // 删除文件以及文件元信息
+	// 动态接口路由设置
+	http.HandleFunc("/file/upload", handler.HTTPInterceptor(handler.UploadHandler))         // 处理上传文件
+	http.HandleFunc("/file/upload/suc", handler.HTTPInterceptor(handler.UploadSucHandler))  // 上传完成
+	http.HandleFunc("/file/meta", handler.HTTPInterceptor(handler.GetFileMetaHandler))      // 获取上传文件元信息
+	http.HandleFunc("/file/query", handler.HTTPInterceptor(handler.FileQueryHandler))       // 批量获取上传文件的元信息
+	http.HandleFunc("/file/download", handler.HTTPInterceptor(handler.DownloadHandler))     // 文件下载
+	http.HandleFunc("/file/update", handler.HTTPInterceptor(handler.FileMetaUpdateHandler)) // 更新文件元信息(重命名)
+	http.HandleFunc("/file/delete", handler.HTTPInterceptor(handler.FileDeleteHandler))     // 删除文件以及文件元信息
+
+	// 秒传接口
 	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler)) // 尝试秒传的接口
 
 	// 分块上传接口
 	http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler)) // 初始化分块上传
 	http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))           // 上传文件分块
-	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))    // 通知完成上传接口
+	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))     // 通知完成上传接口
+	http.HandleFunc("/file/mpload/cancel", handler.HTTPInterceptor(handler.CancelUploadHandler))           // 取消文件上传接口
 
 	// 用户相关接口
 	http.HandleFunc("/user/signup", handler.SignUpHandler)                          // 用户注册
@@ -33,6 +37,7 @@ func main() {
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler)) // 用户信息查询
 
 	// 监听端口
+	fmt.Println("上传服务正在启动, 监听端口:8080...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Fail to start server, err:%s\n", err.Error())
